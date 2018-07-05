a619fd75ce285266b50c5cae84481128b1d2b4be
luke-zhu <luke_zhu@brown.edu>
2018-07-04 23:29:06 -0400

    Remove unnecessary requirements and update CI config

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Build Status](https://travis-ci.com/luke-zhu/my-templates.svg?branch=master)](https://travis-ci.com/luke-zhu/my-templates)
+[![codecov](https://codecov.io/gh/luke-zhu/my-templates/branch/master/graph/badge.svg)](https://codecov.io/gh/luke-zhu/my-templates)
+
+
+# My Templates
+
+Starter code for the types of projects I build.
--- setup.py
+++ setup.py
@@ -1,43 +1,41 @@
 from setuptools import setup, find_packages
 
-# TODO: Figure out a better way to keep this in-sync with the requirements
-microservice_reqs = open('webservice/requirements.txt').read().split('\n')
-dashboard_reqs = []
-etl_reqs = []
+# TODO: Figure out a better way to keep reqs in-sync with the requirements
+util = [
+    'arrow',
+    'fire',
+    'sh',
+]
+webservice = open('webservice/requirements.txt').read().split('\n')
+dashboard = []
+etl = []
+ml = [
+    # Data Science
+    'jupyter',
+    'notebook',
+    'numpy',
+    'scipy',
+    'pandas',
+    'scikit-learn',
+    # Deep Learning
+    'torch',
+    'torchvision',
+    'tensorflow',
+    'keras',
+]
+
+install_reqs = webservice
 
 setup(
     name='my-templates',
     version='0.1.0',
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=[
-        # Utility
-        'arrow',
-        'fire',
-        'sh',
-        # Web
-        'Flask',
-        'Flask-GraphQL',
-        'Flask-SQLAlchemy',
-        'gunicorn',
-        'graphene',
-        'graphene_sqlalchemy',
-        # Data Science
-        'jupyter',
-        'notebook',
-        'numpy',
-        'scipy',
-        'pandas',
-        'scikit-learn',
-        # Deep Learning
-        'torch',
-        'torchvision',
-        'tensorflow',
-        'keras',
-    ],
+    install_requires=webservice,
     extras_require={
         'test': [
             'pytest',
+            'pytest-cov',
             'codecov',
         ]
     }
